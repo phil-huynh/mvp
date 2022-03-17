624cb7b75ee68f1dfaa2dd6348e0ae4770ef8f2c
Phil Huynh <philiphuynh98@gmail.com>
2022-03-16 21:07:51 -0400

    fixed welcome message typo

--- client/src/Welcome.jsx
+++ client/src/Welcome.jsx
@@ -26,7 +26,7 @@ var Welcome = ({showWelcome, handleWelcomeWindow, handleNavChoice}) => {
         >
           <div className="welcome_header toggle_on">Welcome to Strings Theory!</div>
           <div className="upper_welcome_message">The intent of this app is to be a tool that strings players of all skill levels can use to continue to grow and find new sounds. Players can explore new scales or tunings that they have not used before or they can study structures that they already use in greater depth. New features, tunings, scales, and techniques will be added to the app frequently. </div>
-          <div className="middle_welcome_message">This app is currenly still in development with some finished features ready to use. There are tons of exciting features on the horizon. I hope you enjoy the currenly available features. Some upcoming features include modulation tools, modal interchange chords, two-five structures, common cadences, preset progressions, chomatic mediants, savable user preferences, savable progressions, a songwriting tool, a beginner mode, and so much more.</div>
+          <div className="middle_welcome_message">This app is currenly still in development with some finished features ready to use. There are tons of exciting features on the horizon. I hope you enjoy the currenly available features. Some upcoming features include modulation tools, modal interchange chords, two-five structures, common cadences, chromatic mediants, preset progressions, savable user progressions, savable user preferences, a songwriting tool, a beginner mode, and so much more.</div>
           <div className="bottom_welcome_message">In the meantime, try one of these currently available features.</div>
           <div className='construction_options'>
             <span
