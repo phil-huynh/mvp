d78531aa563629c2f311b579a16cb615e14e339f
Phil Huynh <philiphuynh98@gmail.com>
2022-11-30 05:23:41 -0500

    quick edit

--- server/music/Strings.js
+++ server/music/Strings.js
@@ -1,11 +1,6 @@
 const Constants = require('./Constants.js');
-const ChordTypes = require('./ChordTypes.js');
-const ScaleChords = require('./BuildScaleChords.js');
-const AddScales = require('./AddScales.js');
 
 const { chromaticScale, shiftNotes } = Constants;
-const {chordTypes} = ChordTypes;
-const {allScales} = AddScales;
 
 const stringGroups = (() => {
   let [stringGroups, strings, stringsLeft] = [{}, {}, {}];
