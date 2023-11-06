0972252134e51886f436e2449b67866a27b520dc
phil-huynh <philip.huynh@galvanize.com>
2023-11-05 23:13:01 -0500

    updated port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 80;
 // const morgan = require('morgan');
 const Music = require('./music/music_Main.js');
 
